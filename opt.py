ac5ff036408afea948dd26f22741af677c45a77f
Erik Bernhardsson <erikbern@better.com>
2019-10-31 16:22:01 -0400

    do the negative binomial thing for integer distributions

--- opt.py
+++ opt.py
@@ -34,18 +34,30 @@ for c in res.json():
     nqs.append(coalesce(c['bestNoQuantity'], 0))
 
 if all(int(z) == z for z in los):
-    n = 1
+    # It's a bunch of integers: let's model as a negative binomial distribution
+    los, his, yps, nps, yqs, nqs = (numpy.array(z) for z in (los, his, yps, nps, yqs, nqs))
+
+    grid = [(n, p) for n in numpy.exp(numpy.linspace(0, 12, 1000)) for p in numpy.linspace(0, 1, 100)]
+
+    def cdf(x, z):
+        n, p = z
+        return scipy.stats.nbinom.cdf(x, n, p)
+
+    get_probs = lambda z: cdf(his, z) - cdf(numpy.maximum(los-1, 0), z)
+    get_range = lambda: list(range(int(max(los)*1.5)))
+    plot_pdf = lambda z: (get_range(), scipy.stats.nbinom.pmf(get_range(), *z))
 else:
-    # replace every limit by the midpoints
+    # It's a percentage: let's model it as a Beta distribution
+
+    # First, replace every limit by the midpoints
     los2 = [(hi_prev + lo_cur)/200 for lo_cur, hi_prev in zip(los, [0] + his[:-1])]
     his2 = [(hi_cur + lo_next)/200 for hi_cur, lo_next in zip(his, los[1:] + [1])]
     los, his, yps, nps, yqs, nqs = (numpy.array(z) for z in (los2, his2, yps, nps, yqs, nqs))
 
+    # Generate a grid to search on
     g = numpy.exp(numpy.linspace(0, 12, 1000))
     grid = [(p, q) for p in g for q in g if his[0]*0.7 <= p/(p+q) <= los[-1]*1.4]
     print('checking', len(grid), 'params')
-
-    # Use beta distribution
 
     def cdf(x, z):
         a, b = z
@@ -80,14 +92,9 @@ def loss(z, p=False):
 # Grid search to find solution
 s = min(grid, key=loss)
 print('Grid min loss:', s, '->', loss(s))
-s = scipy.optimize.minimize(loss, x0=s).x
-print('Fine tuned loss:', s, '->', loss(s))
+# s = scipy.optimize.minimize(loss, x0=s).x
+# print('Fine tuned loss:', s, '->', loss(s))
 loss(s, True)
-raise
-for k in [1, 2, 3, 5, 8, 10, 15, 20, 30, 40]:
-    s2 = (41.2*k, 58.8*k)
-    print('erik loss:', s2, '->', loss(s2))
-    loss(s2, True)
 xs, ys = plot_pdf(s)
 pyplot.plot(xs, ys)
 pyplot.grid(True)
